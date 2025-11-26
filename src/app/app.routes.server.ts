46af075d3b4ca887c4c55192d32c36d40edc762b
Accelwolf01 <accelwolf01@gmail.com>
2025-11-26 14:26:57 -0500

    Render

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,27 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'admin/usuarios/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'admin/tiendas/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'productos/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'ventas/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'compras/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
